625290dd98c48d7a140b340b76fb5927c17ca327
Amirmohamad <a.hejazirad@dornica.net>
2023-09-13 15:51:07 +0330

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import axios from 'axios';
-import './App.css'
-import imgHome from "./assets/lofi.gif"
+import React   from 'react';
 import {useState} from "react";
+import axios from 'axios';
+import './App.css' 
+import { logo } from './assets';
 
-function App() {
+type TProps = {
+    
+}
+const App : React.FC<TProps> = () => { 
     const [dataProxy , SetDataProxy] = useState([])
     const [ModalWebhook , SetModalWebhook] = useState(false)
     const [Webhook , SetWebhook] = useState({textInput:"" , proxyClick :"", items:{}})
@@ -19,8 +23,10 @@ function App() {
     }
 
 
-    const SendWebHook = (data)=>{
+    const SendWebHook = (data : object)=>{
 
+        console.log(data);
+        
         axios.post(data.textInput, {
             "username": "Proxy",
             "embeds": [
@@ -70,8 +76,8 @@ function App() {
             .catch(function (error) {
                 console.log(error);
             });
-    }
-
+    } 
+    
   return (
     <>
         <div className="container">
@@ -88,7 +94,7 @@ function App() {
 
 
                 {
-                    dataProxy.length === 0 && <img src={imgHome} className="rounded-xl" alt="Cool-Readme" />
+                    dataProxy.length === 0 && <img src={logo} className="rounded-xl" alt="Cool-Readme" />
                 }
 
 
@@ -148,38 +154,7 @@ function App() {
                             </div>
                         </div>
                     </div>
-                </div>
-
-
-                {/*footer*/}
-
-
-
-                {/*<h3>Proje Proxy Tel <h3/> <a href="https://amirmohamadhejazi.github.io/proje_finall/">Link Live</a>*/}
-
-                {/*    <h3 align="left">Open live link, and click get proxy button ... .. . Wait until you receive Tel proxies.. some proxies don't work, test</h3>*/}
-                {/*    <h3 align="left">You can copy the proxy link or save it via the Discord webhook </h3>*/}
-
-                {/*    <h3 align="left">If you like this project, give it a star so that both my energy will increase and other people will use this project <img src="https://user-images.githubusercontent.com/5713670/87202985-820dcb80-c2b6-11ea-9f56-7ec461c497c3.gif" alt="git" width="30" height="30" /></h3>*/}
-                {/*    <img src="https://github.com/mayankchaudhary26/Cool-Readme-ideas/raw/master/data/lofi.gif" alt="tailwind" />*/}
-
-
-
-
-
-
-
-                {/*    <h3 align="left">Languages and Tools:</h3>*/}
-                {/*    <p align="left">*/}
-                {/*        <img src="https://camo.githubusercontent.com/cda2bff49eb0cd388393e08dd91cc3cf461f095e387d3fdcb8648ab0418010aa/68747470733a2f2f692e67697068792e636f6d2f6d656469612f654e41736a4f353574506267616f72376d612f323030772e77656270" alt="css3" width="40" height="40"/><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="40" height="40"/>*/}
-                {/*        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/>*/}
-                {/*        <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/>*/}
-                {/*        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/>*/}
-                {/*        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/>*/}
-                {/*        <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="40" height="40"/>*/}
-                {/*    </p>*/}
-
-
+                </div> 
             </div>
         </div>
 
---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import logo from "./lofi.gif"
+
+export {logo}
